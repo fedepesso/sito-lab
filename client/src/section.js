a6a6557af2f9149fb2d5a3d441872625f84d3095
Tompa02 <30865820+Tompa02@users.noreply.github.com>
2020-07-24 19:23:57 +0200

    Never gonna let you down
    aggiunta funzione di templating

--- client/src/section.js
+++ client/src/section.js
@@ -19,16 +19,16 @@ async function Seeder(tipo){
     let cards = await list.map((e) => {
         return(
             <Card Style='margin: 60px 30px 0px 30px'>
-            <Card.Body>
-                <Card.Title Style='font-size: 35px'>{e.titolo}</Card.Title>
-                <Card.Text Style='font-size: 18px'>
-                {e.descrizione}
-                </Card.Text>
-                <Button variant="danger" Style='font-size:15px' onClick={Viewer}>Visualizza il protoccolo di Laboratorio</Button>
-            </Card.Body>
-            <Card.Footer>
-                Esperienza pensata per classi di {e.classe}
-            </Card.Footer>
+                <Card.Body>
+                    <Card.Title Style='font-size: 35px'>{e.titolo}</Card.Title>
+                    <Card.Text Style='font-size: 18px'>
+                    {e.descrizione}
+                    </Card.Text>
+                    <Button variant="danger" Style='font-size:15px' onClick={Viewer}>Visualizza il protoccolo di Laboratorio</Button>
+                </Card.Body>
+                <Card.Footer>
+                    Esperienza pensata per classi di {e.classe}
+                </Card.Footer>
             </Card>)
     })
 
--- client/src/viewer.js
+++ client/src/viewer.js
@@ -11,10 +11,10 @@ export function Viewer(){
         .then(data => data.json())
         .then(success => {
             ReactDOM.render(
-                <div Style="color : white; text-align : center">
+                <div Style="color : white; text-align : center;">
                     <DefaultNav />
                     <br></br>
-                    {success.data}
+                        {success.data}
                     <br></br>
                     <Button variant="info" Style='font-size:15px' onClick={Section}>Home</Button>
                 </div>,
@@ -23,3 +23,31 @@ export function Viewer(){
         });
 }
 
+
+
+function Template(protocol){
+    return(
+        <div Style='margin : 60px 0px'>
+            <h1 Style='text-align: center'>{protocol.title}</h1>
+            <br></br>
+            <ol Style='text-align: left; color : white;'>
+                <li>
+                    <h2><b>1. Scopo dell'esperimento</b></h2>
+                    <p>{protocol.scope}</p>
+                </li>
+                <li>
+                    <h2><b>2. Materiali</b></h2>
+                    <p>{protocol.materials}</p>
+                </li>
+                <li>
+                    <h2><b>3. Procedimento</b></h2>
+                    <p>{protocol.process}</p>
+                </li>
+                <li>
+                    <h2><b>4. Conclusioni</b></h2>
+                    <p>{protocol.results}</p>
+                </li>
+            </ol>
+        </div>
+    )
+}
